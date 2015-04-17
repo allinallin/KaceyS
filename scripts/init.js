eac286b6f15c0eef1f7a51bc8050d48fdaacd5fc
Allin Resposo <allin@knewton.com>
2015-04-17 15:42:53 -0400

    qa changes

--- scripts/init.js
+++ scripts/init.js
@@ -1,63 +1,47 @@
 module.exports = function(robot) {
-	robot.sessionMapping = robot.sessionMapping || {};
+  robot.sessionMapping = robot.sessionMapping || {};
 
-	// step 1
-	robot.hear(/deploy/i, function(res) {
-		var user = res.message.user.name;
+  robot.hear(/deploy/i, function(res) {
+    var user = res.message.user.name;
 
-		robot.sessionMapping[user] = {
-			step: '1',
-			environment: null
-			service: null
-		};
+    robot.sessionMapping[user] = {
+      step: '1',
+      environment: null,
+      service: null
+    };
 
-		res.send('Ok! Are you deploying a frontend or backend service?');
-	});
+    res.send('Ok! Are you deploying a frontend or backend service?');
+  });
 
-	robot.hear(/frontend/i, function(res) {
-		var user = res.message.user.name;
+  robot.hear(/(frontend|front-end)/i, function(res) {
+    var user = res.message.user.name;
 
-		if (robot.sessionMapping[user] !== '1') {
-			return;
-		}
+    if (robot.sessionMapping[user].step !== '1') {
+      return;
+    }
 
-		robot.sessionMapping[user] = {
-			step: '2',
-			environment: null,
-			service: 'frontend'
-		};
+    robot.sessionMapping[user] = {
+      step: '2',
+      environment: null,
+      service: 'frontend'
+    };
 
-		res.send('Are you deploying to production or QA?');
-	});
+    res.send('Are you deploying to production or QA?');
+  });
 
-	robot.hear(/backend/i, function(res) {
-		var user = res.message.user.name;
+  robot.hear(/(backend|back-end)/i, function(res) {
+    var user = res.message.user.name;
 
-		if (robot.sessionMapping[user] !== '1') {
-			return;
-		}
+    if (robot.sessionMapping[user].step !== '1') {
+      return;
+    }
 
-		robot.sessionMapping[user] = {
-			step: '2',
-			environment: null,
-			service: 'backend'
-		};
+    robot.sessionMapping[user] = {
+      step: '2',
+      environment: null,
+      service: 'backend'
+    };
 
-		res.send('Are you deploying to production or QA?');
-	});
-
-	// prod.js
-
-	// robot.hear(/production/i, function(res) {
-	// 	var user = res.message.user.name;
-
-	// 	if (robot.sessionMapping[user] !== '2') {
-	// 		return;
-	// 	}
-
-	// 	robot.sessionMapping[user].step = 'prod-1';
-	// 	robot.sessionMapping[user].environment = 'prod';
-
-	// 	res.send('Prod question number 1');
-	// });
+    res.send('Are you deploying to production or QA?');
+  });
 };
--- scripts/qa.js
+++ scripts/qa.js
@@ -1,16 +1,78 @@
 module.exports = function(robot) {
-	robot.sessionMapping = robot.sessionMapping || {};
+  robot.sessionMapping = robot.sessionMapping || {};
+  /**
+   * qa-1 Make a release ticket.
+   * qa-2 DB Migration.
+   * qa-3 stack update
+   * qa-4 release to qa
+   * qa-5 integration tests
+   * qa-6 Sanity tests
+   */
 
-	robot.hear(/qa/i, function(res) {
-		var user = res.message.user.name;
+  robot.hear(/qa/i, function(res) {
+    var user = res.message.user.name;
 
-		if (robot.sessionMapping[user] !== '2') {
-			return;
-		}
+    if (robot.sessionMapping[user].step !== '2') {
+      return;
+    }
+    var service = robot.sessionMapping[user].service;
 
-		robot.sessionMapping[user].step = 'qa-1';
-		robot.sessionMapping[user].environment = 'qa';
+    if (service === 'backend') {
+      robot.sessionMapping[user].step = 'qa-1';
+    } else {
+      robot.sessionMapping[user].step = 'qa-2';
+    }
+    res.send('Ok, first thing - open a retail release request ticket in JIRA. Here are some tips that will you fill out some of those mandatory fields. \n *Let me know when you’ve made a ticket!*' +
+    '\nYou can generate a _Changelog_ by running this convenient git command and copying over the relevant commits:' +
+    '\n```git log --pretty=format:\'%h%Creset -%C(yellow)%d%Creset %s - %an (%cr)\'```' +
+    '\nThe _Release Steps_ should look like:' +
+    '\n```kcs s u <stack> <cloudformation template> <environment> [list of variables you wish to override in the cloudformation]```' +
+    '\nFor example:' +
+    '\n```kcs s u Frackend ~/repos/tools/CloudFormation/templates/frackend/Frackend.template RetailQA FrackendVersion=0.2.1449```' +
+    '\nThe _Rollback_ should look the same as the Release Step command but with the desired previous version:' +
+    '\n````kcs s u Frackend ~/repos/tools/CloudFormation/templates/frackend/Frackend.template RetailQA FrackendVersion=0.2.1439```');
+  })
+  function databaseMigration(res) {
 
-		res.send('QA question number 1');
-	});
+  }
+  robot.hear(/(Done|done)/i, function(res) {
+    var user = res.message.user.name;
+    var step = robot.sessionMapping[user].step;
+
+    switch (step) {
+      case 'qa-1':
+        robot.sessionMapping[user].step = 'qa-2';
+        res.send('Awesome. Let\'s run a database migration. The command should look like:' +
+        '\n``` bin/remote_liquibase.sh RetailQA update```' +
+        '\nLet me know when you\'re through.');
+        break;
+      case 'qa-2':
+        robot.sessionMapping[user].step = 'qa-3';
+        res.send('Nice. Let\'s run the stack update. The command should look like:' +
+        '\n```kcs s u <stack> <cloudformation template> <environment> [list of variables you wish to override in the cloudformation]```' +
+        '\nFor example:' +
+        '\n```kcs s u Frackend ~/repos/tools/CloudFormation/templates/frackend/Frackend.template RetailQA FrackendVersion=0.2.1449```');
+        break;
+      case 'qa-3':
+        var service = robot.sessionMapping[user].service;
+
+        if (service === 'backend') {
+          robot.sessionMapping[user].step = 'qa-4';
+        } else {
+          robot.sessionMapping[user].step = 'qa-5';
+        }
+
+        res.send('Now update the status of JIRA ticket. Click _Release to QA_ on the ticket.');
+        break;
+      case 'qa-4':
+        robot.sessionMapping[user].step = 'qa-5';
+        res.send('Run integration tests.');
+        break;
+      case 'qa-5':
+        delete robot.sessionMapping[user];
+
+        res.send('Ok now run a sanity check through the site to make sure nothing\'s broken. You\'re all set after that!');
+        break;
+    }
+  });
 };
