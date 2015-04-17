087ec012982d65a40a53cf0e81ed00e175037de8
Sharanya Venkat <sharanya@knewton.com>
2015-04-17 17:10:18 -0400

    Formatting and copy changes in prod.js
    Signed-off-by: Sharanya Venkat <sharanya@knewton.com>

--- scripts/prod.js
+++ scripts/prod.js
@@ -11,31 +11,31 @@ module.exports = function(robot) {
         robot.sessionMapping[user].step = 'prod-1';
 		robot.sessionMapping[user].environment = 'prod';
 
-        res.send('Cool.Before we continue, please make sure TE have signed off and to check with the doge.');
-        res.send('Udpate the retail release ticket with a snapshot.');
-        res.send('Run `kcs e s <environment> <description>`');
-        res.send('For example: `kcs e s RetailProductionUsEast "Captured snapshot of KERMIT-4044"`');
-        res.send('Please click "Start Deploy" on the release ticket created.');
+        res.send('Cool. Before we continue, please make sure the *Test Engineers* have signed off and *Doge* approves.' +
+        '\nAfter that, we should take a pre-release snapshot in case we have to rollback:' +
+        '\n>Run ```kcs e s <environment> <description>```' +
+        '\n>For example: '+
+        '\n>```kcs e s RetailProductionUsEast "Captured snapshot of KERMIT-4044"```' +
+        '\nWhen you\'re all set, click Start Deploy on the ticket and add the snapshot info to the ticket.');
 
         if (robot.sessionMapping[user].service === 'backend') {
             // Turn on maintenance mode (Optional)
-            res.send('If your release requires a downtime, turn on maintenance mode. To turn it on, do the following:');
-            res.send('* Checkout the proxy sources: `cd ~/repos && kerrit p c services/RetailProxy`');
-            res.send('* cd ~/repos/services/RetailProxy');
-            res.send('* Run `./bin/maintenance.py on <environment>`');
-            res.send('\t\tFor example: `./bin/maintenance.py on dev`');
-            res.send('* Run `./bin/maintenance.py --help for a list of options`');
+            res.send('If your release will result in downtime, turn on maintenance mode by:' +
+            '\n>If needed, checkout the proxy sources: `cd ~/repos && kerrit p c services/RetailProxy`' +
+            '\n>Go into ~/repos/services/RetailProxy' +
+            '\n>Run `./bin/maintenance.py on RetailProductionUsEast`' +
+            '\n>Run `./bin/maintenance.py --help` for a list of options');
 
             // DB Migration
-            res.send('If you need to do DB migration, run the following command:');
-            res.send('`bin/remote_liquibase.sh RetailProductionUsEast update`');
+            res.send('If you need migrate DB, run the following command:' +
+            '\n>```bin/remote_liquibase.sh RetailProductionUsEast update```');
         }
 
         // Stack update
-        res.send('You can now stack update by running the following command:');
-        res.send('`kcs s u <stack> <cloudformation template> <environment> [list of variables you wish to override  in the cloudformation]`');
-        res.send('For example:');
-        res.send('`kcs s u Frackend ~/repos/tools/CloudFormation/templates/frackend/Frackend.template RetailProductionUsEast FrackendVersion=0.2.1449`');
+        res.send('Let\'s run the stack update. The command should look like:'+
+        '\n>```kcs s u <stack> <cloudformation template> <environment> [list of variables you wish to override  in the cloudformation]```' +
+        '\n>For example:' +
+        '\n>```kcs s u Frackend ~/repos/tools/CloudFormation/templates/frackend/Frackend.template RetailProductionUsEast FrackendVersion=0.2.1449```');
 
         // Deploy successful or not
         res.send('Did the deploy succeed?');
@@ -53,15 +53,13 @@ module.exports = function(robot) {
         if (robot.sessionMapping[user].service === 'backend') {
 
             // Turn off maintenance mode (Optional)
-            res.send('If you turned on the maintenance mode in the previous step, turn it off by')
-            res.send('`./bin/maintenance.py off <environment>`');
-            res.send('For example: `./bin/maintenance.py off dev`');
+            res.send('If you turned on the maintenance mode in the previous step, turn it off by running: '+
+            '\n>```./bin/maintenance.py off RetailProductionUsEast```');
         }
 
         // Update the ticket
-        res.send('Click "Release to Production" on the ticket.');
-        res.send('Do not forget to test the changes manually.');
-        res.send('Hurray!! You are done! Congratulations!!!!!!!!!!!!');
+        res.send('Click Release to Production on the ticket.' +
+        '\nOk now run a sanity check through the site to make sure nothing\'s broken. You\'re all set after that!');
 
         delete robot.sessionMapping[user];
     });
