b12b2c887b9459e9f996598f5b646a01de831f8e
Allin Resposo <allin@knewton.com>
2015-04-17 17:11:04 -0400

    more qa copy changes

--- scripts/qa.js
+++ scripts/qa.js
@@ -22,15 +22,16 @@ module.exports = function(robot) {
     } else {
       robot.sessionMapping[user].step = 'qa-2';
     }
-    res.send('Ok, first thing - open a retail release request ticket in JIRA. Here are some tips that will you fill out some of those mandatory fields. \n *Let me know when you’ve made a ticket!*' +
-    '\nYou can generate a _Changelog_ by running this convenient git command and copying over the relevant commits:' +
-    '\n```git log --pretty=format:\'%h%Creset -%C(yellow)%d%Creset %s - %an (%cr)\'```' +
-    '\nThe _Release Steps_ should look like:' +
-    '\n```kcs s u <stack> <cloudformation template> <environment> [list of variables you wish to override in the cloudformation]```' +
-    '\nFor example:' +
-    '\n```kcs s u Frackend ~/repos/tools/CloudFormation/templates/frackend/Frackend.template RetailQA FrackendVersion=0.2.1449```' +
-    '\nThe _Rollback_ should look the same as the Release Step command but with the desired previous version:' +
-    '\n````kcs s u Frackend ~/repos/tools/CloudFormation/templates/frackend/Frackend.template RetailQA FrackendVersion=0.2.1439```');
+    res.send('Ok, first thing - open a retail release request ticket in JIRA. Here are some tips that will you fill out some of those mandatory fields.' +
+    '\nLet me know when you\'re _done_.' +
+    '\n>You can generate a _Changelog_ by running this convenient git command and copying over the relevant commits:' +
+    '\n>```git log --pretty=format:\'%h%Creset -%C(yellow)%d%Creset %s - %an (%cr)\'```' +
+    '\n>The _Release Steps_ should look like:' +
+    '\n>```kcs s u <stack> <cloudformation template> <environment> [list of variables you wish to override in the cloudformation]```' +
+    '\n>For example:' +
+    '\n>```kcs s u Frackend ~/repos/tools/CloudFormation/templates/frackend/Frackend.template RetailQA FrackendVersion=0.2.1449```' +
+    '\n>The _Rollback_ should look the same as the Release Step command but with the desired previous version:' +
+    '\n>```kcs s u Frackend ~/repos/tools/CloudFormation/templates/frackend/Frackend.template RetailQA FrackendVersion=0.2.1439```');
   });
 
   robot.hear(/(Done|done)/i, function(res) {
@@ -41,15 +42,16 @@ module.exports = function(robot) {
       case 'qa-1':
         robot.sessionMapping[user].step = 'qa-2';
         res.send('Awesome. Let\'s run a database migration. The command should look like:' +
-        '\n``` bin/remote_liquibase.sh RetailQA update```' +
-        '\nLet me know when you\'re through.');
+        '\n>``` bin/remote_liquibase.sh RetailQA update```' +
+        '\nLet me know when you\'re _done_.');
         break;
       case 'qa-2':
         robot.sessionMapping[user].step = 'qa-3';
         res.send('Nice. Let\'s run the stack update. The command should look like:' +
-        '\n```kcs s u <stack> <cloudformation template> <environment> [list of variables you wish to override in the cloudformation]```' +
-        '\nFor example:' +
-        '\n```kcs s u Frackend ~/repos/tools/CloudFormation/templates/frackend/Frackend.template RetailQA FrackendVersion=0.2.1449```');
+        '\n>```kcs s u <stack> <cloudformation template> <environment> [list of variables you wish to override in the cloudformation]```' +
+        '\n>For example:' +
+        '\n>```kcs s u Frackend ~/repos/tools/CloudFormation/templates/frackend/Frackend.template RetailQA FrackendVersion=0.2.1449```' +
+        '\nLet me know when you\'re _done_.');
         break;
       case 'qa-3':
         var service = robot.sessionMapping[user].service;
@@ -60,11 +62,13 @@ module.exports = function(robot) {
           robot.sessionMapping[user].step = 'qa-5';
         }
 
-        res.send('Now update the status of JIRA ticket. Click _Release to QA_ on the ticket.');
+        res.send('Now update the status of JIRA ticket. Click _Release to QA_ on the ticket.' +
+        '\nLet me know when you\'re _done_.');
         break;
       case 'qa-4':
         robot.sessionMapping[user].step = 'qa-5';
-        res.send('Run integration tests.');
+        res.send('Run integration tests.' +
+        '\nLet me know when you\'re _done_.');
         break;
       case 'qa-5':
         delete robot.sessionMapping[user];
